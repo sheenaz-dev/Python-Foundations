331468b6e1fd82908c22811ae22a4b932b508d9d
Sheenaz-Salim <snazrin1@gmail.com>
2026-03-02 15:53:32 +0000

    done

---
+++ 05_movieticketgrader.py
@@ -0,0 +1,15 @@
+# --- This mini project - Movie Ticket Grader explain Elif Logic
+
+age = int(input("What is your age? ")) 
+
+if age < 12: 
+
+    print("Your ticket is $5.") 
+
+elif age < 18: 
+
+    print("Your ticket is $10.") 
+
+else: 
+
+    print("Your ticket is $15.")
